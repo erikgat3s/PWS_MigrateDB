b9177d06102c4498f420bf91428c4ec04ef42adf
ERIK <erikgat3s@gmail.com>
2022-10-18 19:58:09 +0700

    1

---
+++ src/main/java/tokosembako/tokobudiman/TokobudimanApplication.java
@@ -0,0 +1,13 @@
+package tokosembako.tokobudiman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokobudimanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokobudimanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tokosembako/tokobudiman/TokobudimanApplicationTests.java
@@ -0,0 +1,13 @@
+package tokosembako.tokobudiman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokobudimanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
